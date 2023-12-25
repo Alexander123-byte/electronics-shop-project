2e367ab756cf804535ce038221a28ec8ef45bfa7
Alexander <san.juckov2017@yandex.ru>
2023-12-26 00:42:26 +0300

    homework_1

--- src/item.py
+++ src/item.py
@@ -12,8 +12,13 @@ class Item:
         :param name: Название товара.
         :param price: Цена за единицу товара.
         :param quantity: Количество товара в магазине.
+        :param id: Способ создания уникального id для подсчета экземпляров класса
         """
-        pass
+        self.name = name
+        self.price = price
+        self.quantity = quantity
+        self.id = len(Item.all) + 1
+        Item.all.append(self)
 
     def calculate_total_price(self) -> float:
         """
@@ -21,10 +26,33 @@ class Item:
 
         :return: Общая стоимость товара.
         """
-        pass
+        return self.price * self.quantity
 
     def apply_discount(self) -> None:
         """
         Применяет установленную скидку для конкретного товара.
         """
-        pass
+        # В данном случае скидка 20%
+        Item.pay_rate = 0.8
+        self.price *= self.pay_rate
+
+
+# Пример использования класса
+if __name__ == "__main__":
+    item1 = Item("Ноутбук", 10000.0, 20)
+    item2 = Item("Смартфон", 5990.0, 35)
+    item3 = Item("Наушники", 1890, 15)
+
+    print(f"В нашем магазине электроники в наличии {len(Item.all)} товаров!\n")
+
+    print(f"Товар {item1.name} с полной стоимостью = {item1.calculate_total_price()}")
+    print(f"Товар {item2.name} с полной стоимостью = {item2.calculate_total_price()}")
+    print(f"Товар {item3.name} с полной стоимостью = {item3.calculate_total_price()}\n")
+
+    item1.apply_discount()
+    item2.apply_discount()
+    item3.apply_discount()
+
+    print(f"Товар {item1.name} с учетом скидки = {item1.price}")
+    print(f"Товар {item2.name} с учетом скидки = {item2.price}")
+    print(f"Товар {item3.name} с учетом скидки = {item3.price}")
--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,34 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
+
+from src.item import Item
+
+
+def test_total_price():
+    """Тестирование расчета стоимости товара без учета скидки"""
+
+    item1 = Item("Ноутбук", 10000.0, 20)
+    item2 = Item("Смартфон", 5990.0, 35)
+    item3 = Item("Наушники", 1890.0, 15)
+
+    # Без скидки
+    assert item1.calculate_total_price() == 200000.0
+    assert item2.calculate_total_price() == 209650.0
+    assert item3.calculate_total_price() == 28350
+
+
+def test_apply_discount():
+    """Тестирование расчета стоимости товара с учетом скидки"""
+    # Устанавливаем свою скидку на товар в размере 20%
+    Item.pay_rate = 0.8
+    item1 = Item("Ноутбук", 10000.0, 20)
+    item2 = Item("Смартфон", 5990.0, 35)
+    item3 = Item("Наушники", 1890.0, 15)
+
+    # Скидка 20%
+    item1.apply_discount()
+    item2.apply_discount()
+    item3.apply_discount()
+
+    assert item1.price == 8000.0
+    assert item2.price == 4792.0
+    assert item3.price == 1512.0
