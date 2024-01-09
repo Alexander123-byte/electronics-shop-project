1decf8e19d301c3f9a3ad4550ff5f02bc652ecad
Alexander <san.juckov2017@yandex.ru>
2024-01-09 22:13:36 +0300

    homework_5

---
+++ src/keyboard.py
@@ -0,0 +1,18 @@
+from src.item import Item
+from src.language_mixin import LanguageMixin
+
+
+class Keyboard(Item, LanguageMixin):
+    def __init__(self, name, price, quantity):
+        super().__init__(name, price, quantity)
+        self.__language = 'EN'
+
+    @property
+    def language(self):
+        return self.__language
+
+    def change_lang(self):
+        if self.__language == 'EN':
+            self.__language = 'RU'
+        else:
+            self.__language = 'EN'
