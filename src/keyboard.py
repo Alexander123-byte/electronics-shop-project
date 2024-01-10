8f90d645f70f9abbad465a21f41de905ea2b4fe1
Alexander <san.juckov2017@yandex.ru>
2024-01-10 16:41:59 +0300

    Edit homework_5

--- src/keyboard.py
+++ src/keyboard.py
@@ -3,15 +3,37 @@ from src.language_mixin import LanguageMixin
 
 
 class Keyboard(Item, LanguageMixin):
+    """
+    Класс для представления клавиатуры в магазине.
+
+    Наследует функционал по хранению и изменению раскладки клавиатуры от LanguageMixin.
+    """
     def __init__(self, name, price, quantity):
+        """
+        Создание экземпляра класса Keyboard.
+
+        :param name: Название клавиатуры.
+        :param price: Цена за единицу клавиатуры.
+        :param quantity: Количество клавиатур в магазине.
+        """
         super().__init__(name, price, quantity)
         self.__language = 'EN'
 
     @property
     def language(self):
+        """
+        Получение текущего языка клавиатуры.
+
+        :return: Текущий язык (EN или RU).
+        """
         return self.__language
 
     def change_lang(self):
+        """
+        Изменение языка клавиатуры.
+
+        Меняет язык с EN на RU и обратно.
+        """
         if self.__language == 'EN':
             self.__language = 'RU'
         else:
--- src/language_mixin.py
+++ src/language_mixin.py
@@ -1,12 +1,32 @@
 class LanguageMixin:
+    """
+    Миксин для добавления фукционала по хранению и изменению раскладки клавиатуры.
+
+    Подмешивается в цепочку наследования класса Keyboard.
+    """
     def __init__(self):
+        """
+        Инициализация миксина.
+
+        Устанавливает язык по умолчанию - английский (EN).
+        """
         self.__language = 'EN'
 
     @property
     def language(self):
+        """
+        Получение текущего языка клавиатуры.
+
+        :return: Текущий язык (EN или RU).
+        """
         return self.__language
 
     def change_lang(self):
+        """
+        Изменение языка клавиатуры.
+
+        Меняет язык с EN на RU и обратно.
+        """
         if self.__language == 'EN':
             self.__language = 'RU'
         else:
